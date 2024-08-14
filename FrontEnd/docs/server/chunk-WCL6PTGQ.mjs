f1df5f5b0db5dfc89d7a66e264f4f615a6664aa9
Mendozaa <orugamendoza@gmail.com>
2024-08-14 08:23:04 +0200

    build

---
+++ FrontEnd/docs/server/chunk-WCL6PTGQ.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UYFIRDFP.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
---
+++ FrontEnd/docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as a}from"./chunk-UUA5G2SR.mjs";import"./chunk-JPC5R4FX.mjs";import"./chunk-RTBQP76W.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-QLAZDZWL.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
